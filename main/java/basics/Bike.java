21ffdf601691819888e61b9b20256206362f9e59
premadevanga <premadevangaa@gmail.com>
2023-05-27 11:19:54 +0530

    push code

---
+++ main/java/basics/Bike.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Bike 
+{
+	Bike(){
+		System.out.println("THAR");
+	}
+	void Color() 
+	{
+		System.out.println("Black");
+	}
+}
---
+++ main/java/basics/Mobile.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Mobile
+{
+	Mobile(){
+		System.out.println("object created");
+	}
+	void Musicplayer() 
+	{
+		System.out.println("welcome to spring");
+	}
+}
